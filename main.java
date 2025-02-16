291476cadb4f42ffbdee81cfedb739abdbfce33c
unknown <syed.raza011@outlook.com>
2025-02-15 20:04:20 -0500

    first commit

---
+++ main.java
@@ -0,0 +1,10 @@
+    public static void main(String[] args) {
+      //System.out.println("Hello world");
+      int[] nums = {1, 2, 3, 4, 5}; // Array with predefined values
+
+      for(int i=0;i<nums.length;i++){
+        System.out.println("Array's"+" "+ i + " "+"element:"+ i);
+      }
+      
+    }
+
